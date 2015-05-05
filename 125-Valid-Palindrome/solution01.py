b0d1cbd225aace82b6b3a8dff67d4f382d944966
Tao Li <eroicacmcs@gmail.com>
2015-05-06 05:35:03 +0800

    #125-Valid-Palindrome DONE, May 2

---
+++ 125-Valid-Palindrome/solution01.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# ==============================================================================
+# Author:   Tao Li (taoli@ucsd.edu)
+# Date:     May 2, 2015
+# Question: 125-Valid-Palindrome
+# Link:     https://leetcode.com/problems/valid-palindrome/
+# ==============================================================================
+# Given a string, determine if it is a palindrome, considering only 
+# alphanumeric characters and ignoring cases.
+# 
+# For example,
+# "A man, a plan, a canal: Panama" is a palindrome.
+# "race a car" is not a palindrome.
+# 
+# Note:
+# Have you consider that the string might be empty? This is a good question 
+# to ask during an interview.
+# 
+# For the purpose of this problem, we define empty string as valid palindrome.
+# ==============================================================================
+
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isPalindrome(self, s):
+        new = "".join([i.lower() for i in s if i.isalpha() or i.isdigit()])
+        size = len(new)
+        i = 0
+        while i < size/2:
+            if new[i] != new[-i-1]:
+                return False
+            i += 1
+        return True
