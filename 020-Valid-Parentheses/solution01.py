e93c642440cc28b0a8d5c4162c81b9c8953bcbaf
Tao Li <eroicacmcs@gmail.com>
2015-05-06 03:26:23 +0800

    #020-Valid-Parentheses DONE, May 3

---
+++ 020-Valid-Parentheses/solution01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# ==============================================================================
+# Author:   Tao Li (taoli@ucsd.edu)
+# Date:     May 3, 2015
+# Question: 020-Valid-Parentheses
+# Link:     https://leetcode.com/problems/valid-parentheses/
+# ==============================================================================
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
+# 
+# The brackets must close in the correct order, "()" and "()[]{}" are all valid 
+# but "(]" and "([)]" are not.
+# ==============================================================================
+
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isValid(self, s):
+        stack = []
+        pairs = {'(': ')', '[': ']', '{': '}'}
+        for i in s:
+            if i in ['(', '[', '{']:
+                stack.append(i)
+            elif i in [')', ']', '}']:
+                if len(stack) == 0:
+                    return False
+                elif pairs[stack[-1]] == i:
+                    stack.pop()
+                else:
+                    return False
+
+        if len(stack) > 0:
+            return False
+        else:
+            return True
