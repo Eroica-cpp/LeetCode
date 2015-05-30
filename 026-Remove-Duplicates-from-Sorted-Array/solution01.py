8ef66bf0f23604e2b864299af14fb28555b48b20
Tao Li <eroicacmcs@gmail.com>
2015-05-30 21:18:44 +0800

    update method desc and complexity analysis

--- 026-Remove-Duplicates-from-Sorted-Array/solution01.py
+++ 026-Remove-Duplicates-from-Sorted-Array/solution01.py
@@ -17,6 +17,10 @@
 # Your function should return length = 2, with the first two elements of nums 
 # being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 # ==============================================================================
+# Method: One pass
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# ==============================================================================
 
 class Solution:
     # @param a list of integers
