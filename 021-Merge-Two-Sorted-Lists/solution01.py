4b425e163bb7b3c6435b4a85e12f86a38c4a5c2d
Tao Li <eroicacmcs@gmail.com>
2015-06-06 03:06:46 +0800

    update complexity analysis

--- 021-Merge-Two-Sorted-Lists/solution01.py
+++ 021-Merge-Two-Sorted-Lists/solution01.py
@@ -8,6 +8,10 @@
 # Merge two sorted linked lists and return it as a new list. The new list should 
 # be made by splicing together the nodes of the first two lists.
 # ==============================================================================
+# Method: Two pointers
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# ==============================================================================
 
 # Definition for singly-linked list.
 # class ListNode:
