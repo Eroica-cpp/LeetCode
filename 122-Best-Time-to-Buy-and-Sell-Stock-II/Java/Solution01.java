d51d65cb03a8d0be32c24054204385a1a19efe03
Tao Li <eroicacmcs@gmail.com>
2015-07-04 02:19:06 +0800

    check extreme case

--- 122-Best-Time-to-Buy-and-Sell-Stock-II/Java/Solution01.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/Java/Solution01.java
@@ -18,6 +18,8 @@
 
 public class Solution01 {
     public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+
         int profit = 0;
         for (int i = 0; i < prices.length-1; i++) {
             if (prices[i+1] - prices[i] > 0) profit += prices[i+1] - prices[i];
