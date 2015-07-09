263f4fed54dc009328d661c3011522cb7e94facb
Tao Li <eroicacmcs@gmail.com>
2015-07-09 18:26:50 +0800

    C version of brute force solution

---
+++ 033-Search-in-Rotated-Sorted-Array/C/Solution02.c
@@ -0,0 +1,38 @@
+/*
+==============================================================================
+Author:   Tao Li (taoli@ucsd.edu)
+Date:     Jul 9, 2015
+Question: 033-Search-in-Rotated-Sorted-Array
+Link:     https://leetcode.com/problems/search-in-rotated-sorted-array/
+==============================================================================
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its 
+index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+==============================================================================
+Method: brute force
+Time Complexity: O(n)
+Space Complexity: O(1)
+==============================================================================
+*/
+
+int search(int* nums, int numsSize, int target) {
+    int i = 0;
+    while (i < numsSize) {
+        if (nums[i] == target) 
+            break;
+        i ++;
+    }
+    return (i < numsSize) ? i : -1;
+}
+
+/* Unit Test */
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
